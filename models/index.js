30e793e342983802b10168b1716cb51780a539dd
Chris Gramer <gramer3514@gmail.com>
2023-08-05 14:32:26 -0700

    brought together models in the index

--- models/index.js
+++ models/index.js
@@ -8,10 +8,20 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, { foreignKey: 'category_id '})
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey: 'category_id'})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
