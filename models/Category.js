a96d0eec72032c7c7c36989adc392962803a1b2f
Chris Gramer <gramer3514@gmail.com>
2023-08-05 14:29:33 -0700

    updated models per readme

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DateTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true,
+    },
+
+    category_name: {
+      type: DateTypes.STRING,
+      allowNull: false,
+    }
+
   },
   {
     sequelize,
