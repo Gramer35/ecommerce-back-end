30b6d7da7f521e6dd8f9b5463993b4e66ed05c06
Chris Gramer <gramer3514@gmail.com>
2023-08-05 17:05:32 -0700

    updated tag routes - used wrong info

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tags = await Product.findAll({ include: [{ model: Product }]});
+    const tags = await Tag.findAll({ include: [{ model: Product }]});
     res.json(tags)
   } catch(err) {
     res.status(500).json('Tags could not be found')
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const oneTag = await Product.findByPk(req.params.id, { include: [{ model: Product }]});
+    const oneTag = await Tag.findByPk(req.params.id, { include: [{ model: Product }]});
     res.json(oneTag)
   } catch(err) {
     res.status(500).json({ message: 'No Tag found with that ID'})
@@ -29,7 +29,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const newTag = await Product.create(req.body);
+    const newTag = await Tag.create(req.body);
     res.json(newTag)
   } catch(err) {
     res.status(500).json({ message: 'New Tag could not be created'})
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const updateTag = await Product.update(req.body, { where: {id: req.params.id}});
+    const updateTag = await Tag.update(req.body, { where: {id: req.params.id}});
     res.json(updateTag)
   } catch(err) {
     res.status(500).json({ message: 'Tag could not be updated'})
@@ -49,7 +49,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const deleteTag = await Product.destroy({ where: {id: req.params.id }});
+    const deleteTag = await Tag.destroy({ where: {id: req.params.id }});
     res.json(deleteTag)
   } catch(err) {
     res.status(500).json({ message: 'Tag could not be deleted' })
